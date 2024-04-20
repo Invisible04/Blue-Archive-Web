cd5ea82290e0eb7d2fe0122b66f5fc74cf9b111d
Shadow77198 <63918277+Invisible04@users.noreply.github.com>
2024-04-20 12:59:12 +0800

    Create main.js

---
+++ system/program/script/other/click animation/main.js
@@ -0,0 +1,22 @@
+let box = document.createElement("div");
+
+box.id = "mouse_effects_box";
+document.body.append(box);
+
+window.document.onclick = (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+    let circle = document.createElement("div");
+
+    circle.classList.add("click_effect");
+
+
+    circle.style.top = y + "px";
+    circle.style.left = x + "px";
+
+    document.getElementById("mouse_effects_box").append(circle);
+    
+    setTimeout(() => {
+        circle.remove();
+    },300);
+}
