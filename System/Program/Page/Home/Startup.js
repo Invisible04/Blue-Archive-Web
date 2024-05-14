888113dfeba9a7641455e7d3b9363622ef141048
Shadow77198 <63918277+Invisible04@users.noreply.github.com>
2024-05-15 01:34:20 +0800

    Add files via upload

---
+++ System/Program/Page/Home/Startup.js
@@ -0,0 +1,31 @@
+const __Elmnt = {
+    NavMainAddBtn: qsa(".add-icon"),
+    NavRecollectionBtn: qs(".recollection-box"),
+    NavMailBtn: qs(".mail-box"),
+    NavMenuBtn: qs(".menu-box"),
+
+    //Custom Element
+    CustomLayer: qs(".custom-layer"),
+    CustomBox: qs(".custom-box"),
+    CustomCloseBtn: qs(".menu-close-btn"),
+    CustomTitle: qs(".title"),
+    CustomText: qs(".title-text"),
+    CustomDeco: qs(".deco"),
+    CustomContainer: qs(".container"),
+    CustomAPCancelBtn: qs(".cappabb-cancel-btn-box"),
+    CustomPSSpecialsBox: qs(".cpscb-specials-container-outer"),
+    CustomPSPyroxeneBox: qs(".cpscb-pyroxene-container-outer"),
+    CustomPSPackagesBox: qs(".cpscb-packages-container-outer"),
+    CustomPSSpecialsBtn: qs(".psnb-tab-box-1"),
+    CustomPSPyroxeneBtn: qs(".psnb-tab-box-2"),
+    CustomPSPackagesBtn: qs(".psnb-tab-box-3"),
+
+    //Custom Container Element
+    CustomMenuContainer: qs(".container-menu"),
+    CustomAPContainer: qs(".container-ap-purchase"),
+    CustomPyroxeneShop: qs(".container-pyroxene-shop"),
+
+    //Custom Full Element
+    CustomFullTitleText: qs(".cntb-title-text"),
+    CustomFullBackBtn: qs(".cnb-back-btn-box")
+};
