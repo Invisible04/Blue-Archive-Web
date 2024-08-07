08f15fdf9cb20d6319eaff95e255c9edbff3367e
Shadow77198 <63918277+Invisible04@users.noreply.github.com>
2024-08-07 20:48:15 +0800

    Add files via upload

---
+++ System/Program/Others/Script/preventContextMenu.js
@@ -0,0 +1,7 @@
+window.addEventListener("contextmenu", (event) => {
+    
+    event.preventDefault();
+    // Optionally, you can add your own logic here
+    // For example, display an alert message
+    // alert("Right Click Blocked!");
+});
